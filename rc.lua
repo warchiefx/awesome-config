e239cdca79aa49f7ff7298e3a8608ebc6cbc1e3a
Ricardo Restituyo <warchiefx@gmail.com>
2017-07-28 19:01:20 -0400

    Disable tyrannical

--- rc.lua
+++ rc.lua
@@ -17,7 +17,7 @@ local beautiful     = require("beautiful")
 local naughty       = require("naughty")
 local lain          = require("lain")
 local cyclefocus = require('cyclefocus')
-local tyrannical = require('tyrannical')
+-- local tyrannical = require('tyrannical')
 local sharedtags = require("sharedtags")
 
 --local menubar       = require("menubar")
@@ -74,33 +74,33 @@ local browser      = "chromium"
 
 awful.util.terminal = terminal
 
--- Ignore the tag "exclusive" property for the following clients (matched by classes)
-tyrannical.properties.intrusive = {
-    "ksnapshot"     , "pinentry"       , "gtksu"     , "kcalc"        , "xcalc"               ,
-    "feh"           , "Gradient editor", "About KDE" , "Paste Special", "Background color"    ,
-    "kcolorchooser" , "plasmoidviewer" , "Xephyr"    , "kruler"       , "plasmaengineexplorer",
-}
+-- -- Ignore the tag "exclusive" property for the following clients (matched by classes)
+-- tyrannical.properties.intrusive = {
+--     "ksnapshot"     , "pinentry"       , "gtksu"     , "kcalc"        , "xcalc"               ,
+--     "feh"           , "Gradient editor", "About KDE" , "Paste Special", "Background color"    ,
+--     "kcolorchooser" , "plasmoidviewer" , "Xephyr"    , "kruler"       , "plasmaengineexplorer",
+-- }
 
--- Ignore the tiled layout for the matching clients
-tyrannical.properties.floating = {
-    "MPlayer"      , "pinentry"        , "ksnapshot"  , "pinentry"     , "gtksu"          ,
-    "xine"         , "feh"             , "kmix"       , "kcalc"        , "xcalc"          ,
-    "yakuake"      , "Select Color$"   , "kruler"     , "kcolorchooser", "Paste Special"  ,
-    "New Form"     , "Insert Picture"  , "kcharselect", "mythfrontend" , "plasmoidviewer"
-}
+-- -- Ignore the tiled layout for the matching clients
+-- tyrannical.properties.floating = {
+--     "MPlayer"      , "pinentry"        , "ksnapshot"  , "pinentry"     , "gtksu"          ,
+--     "xine"         , "feh"             , "kmix"       , "kcalc"        , "xcalc"          ,
+--     "yakuake"      , "Select Color$"   , "kruler"     , "kcolorchooser", "Paste Special"  ,
+--     "New Form"     , "Insert Picture"  , "kcharselect", "mythfrontend" , "plasmoidviewer"
+-- }
 
--- Make the matching clients (by classes) on top of the default layout
-tyrannical.properties.ontop = {
-    "gnome-screenshot"
-}
+-- -- Make the matching clients (by classes) on top of the default layout
+-- tyrannical.properties.ontop = {
+--     "gnome-screenshot"
+-- }
 
--- Force the matching clients (by classes) to be centered on the screen on init
-tyrannical.properties.placement = {
-    kcalc = awful.placement.centered
-}
+-- -- Force the matching clients (by classes) to be centered on the screen on init
+-- tyrannical.properties.placement = {
+--     kcalc = awful.placement.centered
+-- }
 
-tyrannical.settings.block_children_focus_stealing = true --Block popups ()
-tyrannical.settings.group_children = true --Force popups/dialogs to have the same tags as the parent client
+-- tyrannical.settings.block_children_focus_stealing = true --Block popups ()
+-- tyrannical.settings.group_children = true --Force popups/dialogs to have the same tags as the parent client
 
 awful.layout.layouts = {
    awful.layout.suit.floating,
@@ -227,17 +227,19 @@ awful.util.mymainmenu = freedesktop.menu.build({
 
 -- {{{ Screen
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
--- screen.connect_signal("property::geometry", function(s)
---     -- Wallpaper
---     if beautiful.wallpaper then
---         local wallpaper = beautiful.wallpaper
---         -- If wallpaper is a function, call it with the screen
---         if type(wallpaper) == "function" then
---             wallpaper = wallpaper(s)
---         end
---         gears.wallpaper.maximized(wallpaper, s, true)
---     end
--- end)
+screen.connect_signal("property::geometry", function(s)
+    -- Wallpaper
+    -- if beautiful.wallpaper then
+    --     local wallpaper = beautiful.wallpaper
+    --     -- If wallpaper is a function, call it with the screen
+    --     if type(wallpaper) == "function" then
+    --         wallpaper = wallpaper(s)
+    --     end
+    --     gears.wallpaper.maximized(wallpaper, s, true)
+    -- end
+    -- Make nitrogen restore the wallpaper
+    awful.spawn.with_shell('nitrogen --restore')
+end)
 -- Create a wibox for each screen and add it
 awful.screen.connect_for_each_screen(function(s) beautiful.at_screen_connect(s) end)
 -- }}}
