fc5ad9959a22c18af5d6d334977e34dba9e05526
Ricardo Restituyo <warchiefx@gmail.com>
2019-02-09 14:20:32 -0400

    Re-add compton on startup

--- rc.lua
+++ rc.lua
@@ -81,7 +81,7 @@ end
 awesome.connect_signal("startup", function()
                           get_default_app(default_apps, 'browser', 'text/html', 'firefox')
                           run_once({ "nm-applet", "setxkbmap -layout us_intl -option ctrl:swapcaps", "autorandr --change",
-                                     "volumeicon", "gnome-screensaver", "solaar", "xbindkeys"})
+                                     "volumeicon", "gnome-screensaver", "solaar", "xbindkeys", "compton"})
 end)
 
 awesome.connect_signal("exit", function(reason_restart)
