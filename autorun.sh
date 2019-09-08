33cb8e629e53d3c70310c1a642bd25deb19ce89a
Ricardo Restituyo <warchiefx@gmail.com>
2019-09-08 14:44:16 -0400

    Add some new autorun programs

--- autorun.sh
+++ autorun.sh
@@ -7,6 +7,8 @@ function run {
   fi
 }
 
+run /usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run autorandr -c
 run nm-applet
 run volumeicon
